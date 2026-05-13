cddd3b81e228ff2c25301b71ffeed45719a104db
rahul-hasan25 <rahulhaasan2022@gmail.com>
2026-05-13 16:48:49 +0600

    Create Models & Add React Router

--- backend/foodOrderingSystem/admin.py
+++ backend/foodOrderingSystem/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'mobile',
+        'reg_date',
+    )
+    search_fields = (
+        'first_name',
+        'last_name',
+        'email',
+        'mobile',
+    )
+    list_filter = ('reg_date',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'category_name',
+        'creation_date',
+    )
+    search_fields = ('category_name',)
+    list_filter = ('creation_date',)
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'item_name',
+        'category',
+        'item_price',
+        'item_quantity',
+        'is_available',
+    )
+    search_fields = (
+        'item_name',
+        'category__category_name',
+    )
+    list_filter = (
+        'category',
+        'is_available',
+    )
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Food Ordering System
+    </div>
+  )
+}
+
+export default Home
