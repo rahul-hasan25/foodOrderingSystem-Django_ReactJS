8d88250737b53ff217cfe5e0393b6d15d9809018
rahul-hasan25 <rahulhaasan2022@gmail.com>
2026-05-17 01:01:38 +0600

    Responsive Sidebar with Auto Collapse on Mobile

--- frontend/src/components/AdminLayout.js
+++ frontend/src/components/AdminLayout.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import '../styles/adminlogin.css'
 import AdminSidebar from './AdminSidebar'
 import AdminHeader from './AdminHeader'
 
 const AdminLayout = ({children}) => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  useEffect(()=>{
+    const handleResize = () => {
+      if(window.innerWidth < 768) {
+        setSidebarOpen(false);
+      } else {
+        setSidebarOpen(true);
+      }
+    }
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return ()=> window.removeEventListener('resize', handleResize);
+  }, []);
   return (
     <div className='d-flex'>
-      <AdminSidebar/>
-      <div id='page-content-wrapper'>
+      {sidebarOpen && <AdminSidebar/>}
+      <div id='page-content-wrapper' className={`flex-grow-1 ${sidebarOpen ? 'with-sidebar' : 'full-width'}`}>
         <AdminHeader/>
         <div className='container-fluid mt-4'>
           {children}
